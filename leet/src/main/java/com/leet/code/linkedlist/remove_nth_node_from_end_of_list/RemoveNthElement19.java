136c594d1625260104428b1899cdadfa400b98f1
pavel.zhoidz <pavel.zhoidz@altezza.org>
2025-03-24 12:10:48 +0100

    19. Remove Nth Node From End of List

---
+++ leet/src/main/java/com/leet/code/linkedlist/remove_nth_node_from_end_of_list/RemoveNthElement19.java
@@ -0,0 +1,47 @@
+package com.leet.code.linkedlist.remove_nth_node_from_end_of_list;
+
+public class RemoveNthElement19 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode result = new ListNode(0, head);
+        ListNode dummy = result;
+
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
+        dummy.next = dummy.next.next;
+        return result.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ leet/src/main/java/com/leet/code/linkedlist/remove_nth_node_from_end_of_list/task.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/remove-nth-node-from-end-of-list/?envType=study-plan-v2&envId=top-interview-150
